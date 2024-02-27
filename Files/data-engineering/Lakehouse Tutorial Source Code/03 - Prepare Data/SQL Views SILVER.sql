c17d47b2b3aa348d8b043bb5c1357ae8719aa862
nohlimits <matthias.nohl@btelligent.com>
2024-02-27 20:52:35 +0100

    Bugfix View

--- Files/data-engineering/Lakehouse Tutorial Source Code/03 - Prepare Data/SQL Views SILVER.sql
+++ Files/data-engineering/Lakehouse Tutorial Source Code/03 - Prepare Data/SQL Views SILVER.sql
@@ -1,24 +1,24 @@
 SILVER
 CREATE OR ALTER VIEW v_dimension_city AS
-SELECT * FROM [wwilakehouse].[dbo].[dimension_city]
+SELECT * FROM [wwilakehouse_bronze].[dbo].[dimension_city]
 GO
 
 CREATE OR ALTER VIEW v_dimension_customer AS
-SELECT * FROM [wwilakehouse].[dbo].[dimension_customer]
+SELECT * FROM [wwilakehouse_bronze].[dbo].[dimension_customer]
 GO
 
 CREATE OR ALTER VIEW v_dimension_date AS
-SELECT * FROM [wwilakehouse].[dbo].[dimension_date]
+SELECT * FROM [wwilakehouse_bronze].[dbo].[dimension_date]
 GO
 
 CREATE OR ALTER VIEW v_dimension_employee AS
-SELECT * FROM [wwilakehouse].[dbo].[dimension_employee]
+SELECT * FROM [wwilakehouse_bronze].[dbo].[dimension_employee]
 GO
 
 CREATE OR ALTER VIEW v_dimension_stock_item AS
-SELECT * FROM [wwilakehouse].[dbo].[dimension_stock_item]
+SELECT * FROM [wwilakehouse_bronze].[dbo].[dimension_stock_item]
 GO
 
 CREATE OR ALTER VIEW v_fact_sales AS
-SELECT * FROM [wwilakehouse].[dbo].[fact_sale]
+SELECT * FROM [wwilakehouse_bronze].[dbo].[fact_sale]
 GO
